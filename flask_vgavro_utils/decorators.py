e462276895c4c49a071f9ae2733b7b080dcc5e6f
Victor Gavro <vgavro@gmail.com>
2017-07-14 19:42:40 +0300

    views schema decorators: added cached_schema (defaults to true) for invoking new schema per request

--- flask_vgavro_utils/decorators.py
+++ flask_vgavro_utils/decorators.py
@@ -19,48 +19,55 @@ def create_schema(schema_or_dict, extends=None, **kwargs):
     if isinstance(schema_or_dict, type):
         return schema_or_dict(**kwargs)
     elif isinstance(schema_or_dict, dict):
-        return type('_Schema', extends, schema_or_dict)(**kwargs)
+        # NOTE: maybe deepcopy?
+        return type('_Schema', extends, schema_or_dict.copy())(**kwargs)
     else:
         assert isinstance(schema_or_dict, Schema)
         return schema_or_dict
 
 
-def request_schema(schema_or_dict, extends=None, many=None):
-    schema = create_schema(schema_or_dict, extends)
+def request_schema(schema_or_dict, extends=None, many=None, cache_schema=True):
+    schema_ = create_schema(schema_or_dict, extends)
 
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
+            schema = cache_schema and schema_ or create_schema(schema_or_dict, extends)
+
             data = schema.load(request.json, many=many).data
             kwargs.update({'data': data})
             return func(*args, **kwargs)
+
         return wrapper
     return decorator
 
 
-def request_args_schema(schema_or_dict, extends=None):
-    schema = create_schema(schema_or_dict, extends)
+def request_args_schema(schema_or_dict, extends=None, cache_schema=True):
+    schema_ = create_schema(schema_or_dict, extends)
 
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
+            schema = cache_schema and schema_ or create_schema(schema_or_dict, extends)
             request_args = schema.load(request.args).data
             kwargs.update(request_args)
             return func(*args, **kwargs)
+
         return wrapper
     return decorator
 
 
-def response_schema(schema_or_dict, extends=None, many=None):
-    schema = create_schema(schema_or_dict, extends)
+def response_schema(schema_or_dict, extends=None, many=None, cache_schema=True):
+    schema_ = create_schema(schema_or_dict, extends)
 
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
+            schema = cache_schema and schema_ or create_schema(schema_or_dict, extends)
             result = func(*args, **kwargs)
             if isinstance(result, (list, tuple)) and (schema.many or many):
                 return schema.dump(result, many=many).data
             return schema.dump(result, many=many).data
-            return result
+
         return wrapper
     return decorator
