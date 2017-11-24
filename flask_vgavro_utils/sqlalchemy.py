e6ac1b57df7c556b4199ebcacb6b2b7e6db51be4
maksim.kondel <maksim.kondel@gmail.com>
2017-11-24 19:40:16 +0200

    Refactored apply_driver_hacks

--- flask_vgavro_utils/sqlalchemy.py
+++ flask_vgavro_utils/sqlalchemy.py
@@ -7,20 +7,18 @@ class SQLAlchemy(SQLAlchemy):
     """
     Extends driver and session with custom options.
     """
-    def __init__(app=None, use_native_unicode=True, session_options={}, **kwargs):
+    def __init__(self, app=None, use_native_unicode=True, session_options={}, **kwargs):
         if app and 'SQLALCHEMY_AUTOCOMMIT' in app.config:
             session_options['autocommit'] = app.config['SQLALCHEMY_AUTOCOMMIT']
         super().__init__(app, use_native_unicode, session_options, **kwargs)
 
     def apply_driver_hacks(self, app, info, options):
-        rv = super().apply_driver_hacks(app, info, options)
+        super().apply_driver_hacks(app, info, options)
         if (app and 'SQLALCHEMY_STATEMENT_TIMEOUT' in app.config and
            info.drivername == 'postgresql'):
-            timeout = int(app.config['SQLALCHEMY_STATEMENT_TIMEOUT'])
-            rv.setdefault('connect_args', {})
-            rv['connect_args'].setdefault('options', {})
-            rv['connect_args']['options'] = '-c statement_timeout={:d}'.format(timeout)
-        return rv
+            timeout = int(app.config['SQLALCHEMY_STATEMENT_TIMEOUT']) * 1000
+            assert 'connect_args' not in options
+            options['connect_args'] = {'options': '-c statement_timeout={:d}'.format(timeout)}
 
 
 class ModelReprMixin(ReprMixin):
