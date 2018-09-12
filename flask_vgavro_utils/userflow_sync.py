073bf3935109d3708a7b11b8f8dbf78eca72132e
Victor Gavro <vgavro@gmail.com>
2018-09-12 19:37:17 +0300

    userflow sync: some api changes

--- flask_vgavro_utils/userflow_sync.py
+++ flask_vgavro_utils/userflow_sync.py
@@ -4,6 +4,8 @@ import sqlalchemy as sa
 from dateutil.parser import parse as dateutil_parse
 from flask import current_app
 
+from .exceptions import ImproperlyConfigured
+
 
 class SyncMixin:
     # date of sync time from userflow
@@ -12,19 +14,43 @@ class SyncMixin:
     sync_need = sa.Column(sa.Boolean(), nullable=True)
 
 
+def _maybe_flat_to_dict(data):
+    return {f: f for f in data} if isinstance(data, (tuple, list, set)) else data
+
+
+def _synchronizer_meth_decorator(type_):
+    def decorator(func):
+        func._synchronizer = {'type': type_, 'name': func.__name__}
+        return func
+
+
 class Synchronizer:
     model = None
-    get_fields = []
-    set_fields = []
+    id_attr = 'id'
+    getters = {}  # field name, field name or callable(instance)
+    setters = {}  # field name, field name or callable(
     allow_create = False
 
-    def __init__(self, model=None, get_fields=None, set_fields=None, allow_create=None):
+    getter = staticmethod(_synchronizer_meth_decorator('getters'))
+    setter = staticmethod(_synchronizer_meth_decorator('setters'))
+
+    def __init__(self, model=None, getters=None, setters=None, allow_create=None):
         self.model = model or self.model
         assert issubclass(self.model, SyncMixin)
-        self.get_fields = get_fields or self.get_fields
-        self.set_fields = set_fields or self.set_fields   # TODO: do we need it here?
-        assert self.get_fields or self.set_fields
+
+        self.getters = _maybe_flat_to_dict(getters or self.getters)
+        self.setters = _maybe_flat_to_dict(setters or self.setters)
+        for attr in self.__dict__.values():
+            if callable(attr) and hasattr(attr, '_synchronizer'):
+                params = attr._synchronizer
+                getattr(self, params['type'])[params['name']] = attr
+        assert self.getters or self.setters
+
         self.allow_create = allow_create if allow_create is not None else self.allow_create
+
+    @property
+    def name(self):
+        return self.model.__name__.lower()
 
     @property
     def session(self):
@@ -36,7 +62,7 @@ class Synchronizer:
         ids = set(map(str, ids))
 
         rv = {str(obj.id): obj for obj in
-              self.model.query.filter(self.model.id.in_(ids))}
+              self.model.query.filter(getattr(self.model, self.id_attr).in_(ids))}
 
         if self.allow_create:
             for id in ids.difference(rv.keys()):
@@ -45,7 +71,7 @@ class Synchronizer:
         return rv
 
     def create_instance(self, id):
-        instance = self.model(id=id)
+        instance = self.model(**{self.id_attr: id})
         self.session.add(instance)
         return instance
 
@@ -56,41 +82,50 @@ class Synchronizer:
         pass
 
     def set(self, instance, data, time):
-        self._set_fields(instance, data, time)
+        self._set_data(instance, data, time)
         instance.synced_at = time
 
-    def _set_fields(self, instance, data, time):
+    def _set_data(self, instance, data, time):
         for field in data:
             self._set_field(instance, field, data[field], time)
 
     def _set_field(self, instance, field, value, time):
-        assert field in self.set_fields
-        if hasattr(instance, field):
-            setattr(instance, field, value)
+        setter = self.setters[field]
+        if callable(setter):
+            setter(instance, value)
         else:
-            instance.data[field] = value
+            if hasattr(instance, setter):
+                setattr(instance, setter, value)
+            else:
+                instance.data[setter] = value
 
     def get(self, instance):
         instance.sync_need = False
-        return self._get_fields(instance)
+        return self._get_data(instance)
 
-    def _get_fields(self, instance):
+    def _get_data(self, instance):
         return {field: self._get_field(instance, field)
-                for field in self.get_fields}
+                for field in self.getters}
 
     def _get_field(self, instance, field):
-        if hasattr(instance, field):
-            return getattr(instance, field)
+        getter = self.getters[field]
+        if callable(getter):
+            return getter(instance)
         else:
-            return instance.data.get(field)
+            if hasattr(instance, field):
+                return getattr(instance, field)
+            else:
+                return instance.data.get(field)
 
 
 def map_synchronizers(synchronizers):
     rv = {}
-    for synchronizer in synchronizers:
-        name = synchronizer.model.__name__.lower()
-        assert name not in rv, 'Synchronizer already registered: {}'.format(name)
-        rv[name] = synchronizer
+    for s in synchronizers:
+        if isinstance(s, type):
+            s = s()
+        if s.name in rv:
+            raise ImproperlyConfigured('Synchronizer already registered: {}'.format(s.name))
+        rv[s.name] = s
     return rv
 
 
@@ -119,7 +154,7 @@ def sync_response(synchronizers, data, session=None):
     return rv
 
 
-def sync_request(synchronizers, real_request, data, time=None):
+def sync_request(synchronizers, request, data, time=None):
     payload = {'time': (time or datetime.utcnow()).isoformat()}
     data_map = {}
 
@@ -129,14 +164,17 @@ def sync_request(synchronizers, real_request, data, time=None):
         if not isinstance(ids_or_instances[0], SyncMixin):
             data_map[name] = synchronizer.get_instances(ids_or_instances)
         else:
-            data_map[name] = {str(instance.id): instance for instance in ids_or_instances}
+            data_map[name] = {
+                str(getattr(instance, synchronizer.id_attr)): instance
+                for instance in ids_or_instances
+            }
 
         payload[name] = {}
         for id, instance in data_map[name].items():
             synchronizer.preprocess(instance)
             payload[name][id] = synchronizer.get(instance)
 
-    response = real_request(payload)
+    response = request(payload)
 
     time = dateutil_parse(response['time'])
     for name, instance_map in data_map.items():
